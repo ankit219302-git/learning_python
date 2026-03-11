c05f3a84262177e38215a35cbd180a520dac334a
ankit219302-git <ankit219302@gmail.com>
2026-03-12 02:27:13 +0530

    code refactor

--- imageprocessor/blur.py
+++ imageprocessor/blur.py
@@ -3,4 +3,4 @@ from PIL import ImageFilter, Image
 before = Image.open("resources/dog.jpg")
 after = before.filter(ImageFilter.BoxBlur(5))
 # after = before.filter(ImageFilter.GaussianBlur(radius=4))
-after.save("resources/dog-blurred.jpg")
+after.save("resources/dog_blurred.jpg")
--- imageprocessor/edge.py
+++ imageprocessor/edge.py
@@ -2,4 +2,4 @@ from PIL import Image, ImageFilter
 
 before = Image.open("resources/sunset.jpg")
 after = before.filter(ImageFilter.FIND_EDGES)
-after.save("resources/sunset-edges.jpg")
+after.save("resources/sunset_edges.jpg")
