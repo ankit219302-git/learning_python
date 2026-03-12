ce2143993be98c5c98e4b30f25e8e1db41b602e9
ankit219302-git <ankit219302@gmail.com>
2026-03-12 12:22:34 +0530

    calculator

--- src/calculator/simple_calculator.py
+++ src/calculator/simple_calculator.py
@@ -1,4 +1,14 @@
 x = int(input("x: "))
 y = int(input("y: "))
+operation = input("Operation (+,-,/,x): ")
 
-print("The sum is:", x + y)
+if operation == "+":
+    print("The sum is:", x + y)
+elif operation == "-":
+    print("The difference is:", x - y)
+elif operation == "/":
+    print("The division quotient is:", x / y)
+elif operation == "x":
+    print("The product is:", x * y)
+else:
+    print("Invalid operation")
