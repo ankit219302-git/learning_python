31ce7d071070ffeafa4e0a949a6900af414f4cd0
ankit219302-git <ankit219302@gmail.com>
2026-04-06 19:08:51 +0530

    updated loops learning

--- src/loop/for_range.py
+++ src/loop/for_range.py
@@ -37,3 +37,10 @@ print()
 print("After (starting from -2 index): ", end="")
 for i in range(-2, len(before)):                    # This (-2) value can't be less than the input word length else out-of-bounds error will be thrown
     print(before[i].upper(), end="")
+
+print()
+print("After (increasing 2 indices at a time): ", end="")
+# This will increment the loop by 2 provided as the 3rd param of range function
+# 0 need not be given explicitly as the starting index is 0 by default
+for i in range(0, len(before), 2):
+    print(before[i].upper(), end="")
